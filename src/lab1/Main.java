1d113bae101161eb13725c44c54624995c1bd4aa
Joe <joex.lin@mail.utoronto.ca>
2025-09-08 10:22:58 -0400

    Created Main.java

---
+++ src/lab1/Main.java
@@ -0,0 +1,10 @@
+package lab1;
+public class Main {
+    public static void main(String[] args) {
+        String[] argument = {};
+        FizzBuzz.main(argument);
+        //FizzBuzz.doFizzBuzz(5);
+    }
+
+}
+
