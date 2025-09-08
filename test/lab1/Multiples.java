0263d8de21aed71883e3dc7030584af4495429e4
Joe <joex.lin@mail.utoronto.ca>
2025-09-08 10:59:59 -0400

    Created Multiples.java

---
+++ test/lab1/Multiples.java
@@ -0,0 +1,21 @@
+package lab1;
+
+public class Multiples {
+    public static void main(String[] args) {
+        System.out.println(multiples(1000, 3, 5));
+    }
+
+    public static int multiples(int n, int a, int b) {
+        int count = 0;
+        for(int i = 1; i < n; i++){
+
+            boolean divisibleByA = i % a == 0;
+            boolean divisibleByB = i % b == 0;
+            if(divisibleByA || divisibleByB){
+                count++;
+            }
+        }
+        return count;
+
+    }
+}
